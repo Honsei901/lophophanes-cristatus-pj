507684c3c49d64a3e0672acc53f6d153fbd384cb
Honsei901 <homma_wic@outlook.jp>
2024-10-10 00:39:11 +0900

    Set up cors setting

--- src/main/java/com/example/spring_curd/repository/UserRepository.java
+++ src/main/java/com/example/spring_curd/repository/UserRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.spring_curd.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
 }
